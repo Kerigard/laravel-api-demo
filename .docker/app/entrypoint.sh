84dc29894802f3cf7f5bcd73f5c9f099a8e1682e
Kerigard <2fenix7@gmail.com>
2025-05-05 20:26:33 +0300

    build: add docker configs

---
+++ .docker/app/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+cd /var/www/html
+
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+if [[ "$BUILD_ENV" != "production" && ! -d "vendor" ]]; then
+  composer install --no-interaction --no-progress
+fi
+
+if ! grep -q "^APP_KEY=" .env || grep -q "^APP_KEY=$" .env; then
+  php artisan key:generate
+fi
+
+if [ "$BUILD_ENV" = "production" ]; then
+  php artisan config:cache
+  php artisan route:cache
+  php artisan view:cache
+fi
+
+exec "$@"
