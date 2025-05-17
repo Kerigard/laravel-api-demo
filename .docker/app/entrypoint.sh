6f9f8ee8a6d1ee9c46fc731dd8d2d29dd9768d02
Kerigard <2fenix7@gmail.com>
2025-05-17 17:06:06 +0300

    build: generate jwt secret during the first build

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -15,6 +15,10 @@ if ! grep -q "^APP_KEY=" .env || grep -q "^APP_KEY=$" .env; then
   php artisan key:generate
 fi
 
+if ! grep -q "^JWT_SECRET=" .env || grep -q "^JWT_SECRET=$" .env; then
+  php artisan jwt:secret -f
+fi
+
 if [ "$BUILD_ENV" = "production" ]; then
   php artisan config:cache
   php artisan route:cache
